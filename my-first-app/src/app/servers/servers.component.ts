1b6a7b220058badc954aa4b6351954613d99c54d
Jason <sysbender@gmail.com>
2022-11-20 16:45:58 -0500

    combinding all forms of data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -20,7 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created : name is ' + this.serverName;
   }
 
   OnUpdateServerName(event: Event){
